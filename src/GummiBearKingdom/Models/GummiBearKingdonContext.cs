9186d1a7af848aa5187162a273ac7faff07d8b55
Kory <koryskarbek@yahoo.com>
2017-04-21 08:28:27 -0700

    before running migration commads to create database

---
+++ src/GummiBearKingdom/Models/GummiBearKingdonContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GummiBearKingdom.Models
+{
+    public class GummiBearKingdonContext : DbContext
+    {
+        public GummiBearKingdonContext()
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummiBearKingdom;integrated security=True");
+        }
+
+        public GummiBearKingdonContext(DbContextOptions<GummiBearKingdonContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ src/GummiBearKingdom/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GummiBearKingdom.Models
+{
+    [Table("Products")]
+    public class Products
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string CountryOfOrigin { get; set; }
+    }
+}
